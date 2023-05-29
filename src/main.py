cdbba6bf0c563ae5397f0156db66e06906d450dc
Anatolii Kostrygin <anatolii.kostrygin@gmail.com>
2023-05-29 15:50:10 +0200

    feat: Target evaluation. At least wins against PythonSlayer for seed=-3226224768081418000

--- src/main.py
+++ src/main.py
@@ -30,11 +30,13 @@ def echo_input():
 
 class Cell:
     def __init__(
-        self, _type: int, resources: int, neighbors: tuple[int, int, int, int, int, int]
+        self, _type: int, resources: int, neighbors: tuple[int, int, int, int, int, int], my_ants: int, opp_ants: int
     ):
         self._type = _type
         self.resources = resources
         self.neighbors = neighbors
+        self.my_ants = my_ants
+        self.opp_ants = opp_ants
 
     def is_egg(self) -> bool:
         return self._type == EGG
@@ -50,6 +52,8 @@ class Field:
         self.opp_base_index = opp_base_index
 
         self.distances = [self._build_distances_from(i) for i in range(self.size)]
+        self.total_my_ants = 0
+        self.total_opp_ants = 0
 
     @property
     def size(self) -> int:
@@ -77,6 +81,23 @@ class Field:
             i += 1
         return distances
 
+    def cell_is_mine(self, i_cell) -> bool:
+        return self.distances[i_cell][self.my_base_index] < self.distances[i_cell][self.opp_base_index]
+
+    def cell_is_neutral(self, i_cell) -> bool:
+        return self.distances[i_cell][self.my_base_index] == self.distances[i_cell][self.opp_base_index]
+
+    def apply_update(self, upd: list[tuple[int, int, int]]):
+        self.total_my_ants = 0
+        self.total_opp_ants = 0
+        for (resources, my_ants, opp_ants), cell in zip(upd, self.cells):
+            cell.resources = resources
+            cell.my_ants = my_ants
+            cell.opp_ants = opp_ants
+            self.total_my_ants += my_ants
+            self.total_opp_ants += opp_ants
+
+
 def read_initial() -> Field:
     number_of_cells = int(echo_input())
     cells = []
@@ -96,6 +117,8 @@ def read_initial() -> Field:
                 _type=_type,
                 resources=initial_resources,
                 neighbors=(neigh_0, neigh_1, neigh_2, neigh_3, neigh_4, neigh_5),
+                my_ants=0,
+                opp_ants=0
             )
         )
     number_of_bases = int(echo_input())
@@ -140,34 +163,89 @@ def find_target(
     return best_target
 
 
+def evaluate_target(field: Field, i_cell: int) -> tuple[int, float]:
+    """ Max is better. First element is negative and is a priority (-1 == top-priority). 2nd is a "geometric" score. """
+    cell = field.cells[i_cell]
+    my_dist = field.distances[i_cell][field.my_base_index]
+    dist_ratio = my_dist / field.distances[i_cell][field.opp_base_index]
+
+    if cell.is_egg() and dist_ratio <= 0.66:
+        return -1, -my_dist
+    if cell.is_crystal() and 0.34 <= dist_ratio <= 1:
+        return -2, time_to_harvest(my_dist, cell.resources, field.total_my_ants)
+    if cell.is_egg() and dist_ratio <= 1.3:
+        return -3, my_dist
+    elif cell.is_crystal() and dist_ratio > 1:
+        return -4, -my_dist
+    elif cell.is_crystal() and dist_ratio < 0.34:
+        return -5, 0
+    return -7, 0  # lower priority for any other case
+
+
 if __name__ == "__main__":
     field = read_initial()
     distances_from_my_base = field.distances[field.my_base_index]
     expected_length = None
     i_move = 0
     target = None
+    my_cells = set()
+    for i, cell in enumerate(field.cells):
+        if cell.resources > 0 and field.cell_is_mine(i):
+            debug(f"Cell {i} is mine")
+            my_cells.add(i)
+
     # game loop
     while True:
         i_move += 1
-        targets = []
         dist_to_resources = {}
         curr_state = []
         total_my_ants = 0
         total_opp_ants = 0
+        upd = []
         for i in range(field.size):
             resources, my_ants, opp_ants = [int(j) for j in echo_input().split()]
-            total_my_ants += my_ants
-            total_opp_ants += opp_ants
-            curr_state.append((resources, my_ants, opp_ants))
+            upd.append((resources, my_ants, opp_ants))
+        field.apply_update(upd)
 
-        if expected_length is None:
-            expected_length = field.compute_expected_length(total_my_ants + total_opp_ants)
-            debug(f"{expected_length = }")
+        #     total_my_ants += my_ants
+        #     total_opp_ants += opp_ants
+        #     curr_state.append((resources, my_ants, opp_ants))
+        #
+        # if expected_length is None:
+        #     expected_length = field.compute_expected_length(total_my_ants + total_opp_ants)
+        #     debug(f"{expected_length = }")
+        #
+        # if target is None or curr_state[target][0] == 0:
+        #     target = find_target(field, distances_from_my_base, curr_state, total_my_ants, i_move, expected_length)
+        #
+        # if target != -1:
+        #     print(f"LINE {field.my_base_index} {target} {1}")
+        # else:
+        #     print("WAIT")
 
-        if target is None or curr_state[target][0] == 0:
-            target = find_target(field, distances_from_my_base, curr_state, total_my_ants, i_move, expected_length)
+        raw_targets = []
+        for i, cell in enumerate(field.cells):
+            if cell.resources > 0:
+                score = evaluate_target(field, i)
+                raw_targets.append((score, i))
+        targets = []
+        total_dist = 0
+        for score, i_cell in sorted(raw_targets, reverse=True):
+            distance_to_target = field.distances[i_cell][field.my_base_index]
+            if total_dist + distance_to_target <= field.total_my_ants:
+                targets.append((i_cell, 100))
+                total_dist += distance_to_target
+            # if cell.resources > 0 and i in my_cells:
+            #     if field.cells[i].is_crystal() and i_move >= 4:
+            #         targets.append((i, cell.resources))
+            #     elif i_move <= 5 and field.cells[i].is_egg():
+            #         targets.append((i, cell.resources))
+            #     elif field.cells[i].is_egg():
+            #         targets.append((i, cell.resources // 2))
 
-        if target != -1:
-            print(f"LINE {field.my_base_index} {target} {1}")
+        debug(f"{targets =}")
+        if targets:
+            actions = ';'.join(f"LINE {field.my_base_index} {i} {resources}" for i, resources in targets)
         else:
-            print("WAIT")
+            actions = "WAIT"
+        print(actions)
